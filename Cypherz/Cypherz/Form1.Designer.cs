ef6c002382a595148ea317ad03b98576982fddea
tgilmore9812 <62147376+tgilmore9812@users.noreply.github.com>
2020-03-13 11:08:29 -0400

    Add files via upload

---
+++ Cypherz/Cypherz/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Cypherz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.namePanel = new System.Windows.Forms.Panel();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.middleNameText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.mastersDegreeCheck = new System.Windows.Forms.CheckBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.namePanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // namePanel
+            // 
+            this.namePanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.namePanel.Controls.Add(this.lastNameText);
+            this.namePanel.Controls.Add(this.label3);
+            this.namePanel.Controls.Add(this.middleNameText);
+            this.namePanel.Controls.Add(this.label2);
+            this.namePanel.Controls.Add(this.firstNameText);
+            this.namePanel.Controls.Add(this.label1);
+            this.namePanel.Location = new System.Drawing.Point(12, 12);
+            this.namePanel.Name = "namePanel";
+            this.namePanel.Size = new System.Drawing.Size(341, 100);
+            this.namePanel.TabIndex = 1;
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(99, 66);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(225, 22);
+            this.lastNameText.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name";
+            // 
+            // middleNameText
+            // 
+            this.middleNameText.Location = new System.Drawing.Point(99, 35);
+            this.middleNameText.Name = "middleNameText";
+            this.middleNameText.Size = new System.Drawing.Size(225, 22);
+            this.middleNameText.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Middle Name";
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(99, 6);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(225, 22);
+            this.firstNameText.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.checkBox6);
+            this.panel2.Controls.Add(this.checkBox5);
+            this.panel2.Controls.Add(this.checkBox4);
+            this.panel2.Controls.Add(this.checkBox3);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.mastersDegreeCheck);
+            this.panel2.Location = new System.Drawing.Point(13, 119);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(340, 110);
+            this.panel2.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "CS Language Certificates";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Computer Science Degree";
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(168, 103);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(155, 21);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "Web Dev Certificate";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(3, 103);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(122, 21);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "C++ Certificate";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(168, 76);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(127, 21);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Java Certificate";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(3, 76);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(114, 21);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "C# Certificate";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(168, 32);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(144, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Bachelors Degree";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // mastersDegreeCheck
+            // 
+            this.mastersDegreeCheck.AutoSize = true;
+            this.mastersDegreeCheck.Location = new System.Drawing.Point(5, 32);
+            this.mastersDegreeCheck.Name = "mastersDegreeCheck";
+            this.mastersDegreeCheck.Size = new System.Drawing.Size(131, 21);
+            this.mastersDegreeCheck.TabIndex = 0;
+            this.mastersDegreeCheck.Text = "Masters Degree";
+            this.mastersDegreeCheck.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(12, 235);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 3;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(278, 235);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 450);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.namePanel);
+            this.Name = "Form1";
+            this.Text = "Cypherz";
+            this.namePanel.ResumeLayout(false);
+            this.namePanel.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel namePanel;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox middleNameText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox firstNameText;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox mastersDegreeCheck;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Cypherz/Cypherz/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using FireSharp.Extensions;
+
+
+namespace Cypherz
+{
+
+    public partial class Form1 : Form
+    {
+        
+        IFirebaseConfig config = new FirebaseConfig()
+        {
+            AuthSecret = "uaV8UxYoFmLHWMBDB3n7rLPvecjKqCJLVBjBodhn",
+            BasePath = "https://bcas-d6975.firebaseio.com/"
+
+        };
+        
+        IFirebaseClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new FireSharp.FirebaseClient(config);
+        }
+        //submit button checks to see if the name fields are empty
+        private async void submitButton_Click(object sender, EventArgs e)
+        {
+
+            foreach (Panel pnl in Controls.OfType<Panel>())
+            {
+                foreach (TextBox tb in pnl.Controls.OfType<TextBox>())
+                {
+                    if (string.IsNullOrEmpty(tb.Text.Trim()))
+                    {
+                        MessageBox.Show("Ensure all data fields are filled");
+                        return;
+                    }
+                }
+            }
+
+            string Creds="";
+            foreach(Panel pnl in Controls.OfType<Panel>())
+            {
+                foreach(CheckBox chk in pnl.Controls.OfType<CheckBox>())
+                {
+                    if (chk.Checked)
+                    {
+                        Creds += "\'"+chk.Text+"\'";
+                    }
+                }
+            }
+            MessageBox.Show(Creds);
+            var data = new Teacher
+            {
+                credentials = Creds,
+                lastName = lastNameText.Text,
+                middleName = middleNameText.Text,
+                firstName = firstNameText.Text,
+            };
+            SetResponse response = await client.SetTaskAsync("Teachers/" + firstNameText.Text+" "+lastNameText.Text,data);
+            Teacher result = response.ResultAs<Teacher>();
+            MessageBox.Show("Data Inserted: " + result.firstName);
+
+    }
+
+        private async void resetButton_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse response = await client.GetTaskAsync("Teachers/"+firstNameText.Text+" "+lastNameText.Text);
+            Teacher teacher = response.ResultAs<Teacher>();
+            MessageBox.Show("Teacher: "+teacher.firstName+teacher.lastName+"\nCredentials: "+teacher.credentials);
+            
+        }
+    }
+}
