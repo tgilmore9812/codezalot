aa1b18a048d7d078b00ab24b69c263dd94a5445a
tgilmore9812 <62147376+tgilmore9812@users.noreply.github.com>
2020-03-13 11:19:54 -0400

    Update Form1.cs

--- Cypherz/Cypherz/Form1.cs
+++ Cypherz/Cypherz/Form1.cs
@@ -63,7 +63,6 @@ namespace Cypherz
                     }
                 }
             }
-            MessageBox.Show(Creds);
             var data = new Teacher
             {
                 credentials = Creds,
